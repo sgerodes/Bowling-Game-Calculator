70caf3bbd03d0cb8a76f891bed345d242d7a77d9
Sergey Gerodes <sgerodes@gmail.com>
2021-04-12 12:04:41 +0200

    add swagger

---
+++ src/main/java/com/sgerodes/bowlinggame/configuration/SpringFoxConfig.java
@@ -0,0 +1,38 @@
+package com.sgerodes.bowlinggame.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sgerodes"))
+                .paths(PathSelectors.ant("/api/*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Bowling Game Calculator API",
+                "An api for calculating the overall score in a bowling game",
+                "1.0",
+                "Free to use",
+                new Contact("Sergey Gerodes", "www.example.com", "myeaddress@company.com"),
+                "API License",
+                "API license URL",
+                Collections.emptyList());
+    }
+}
